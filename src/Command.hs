7e203f86e298ca4fa62403f735797757c0fc0aa1
Jean-Baptiste Potonnier <jean.baptiste.potonnier@gmail.com>
2015-03-24 16:31:40 +0100

    après atelier

--- src/Command.hs
+++ src/Command.hs
@@ -9,36 +9,74 @@ import           Control.Applicative ((*>), (<$), (<$>), (<*), (<*>))
 import           Data.Text           (Text)
 import qualified Data.Text           as T
 import           Text.Parsec         (ParseError, choice, digit, many1, oneOf,
-                                      parse, string)
+                                      parse, string, sepBy, try)
 import           Text.Parsec.Text    (Parser)
+import Data.List (intercalate)
 
-data Command = Add Int Int
-               | Name
+data Command = Largest [Int]
+               | Add Int Int
+               | Mult Int Int
+               | Minus Int Int
+               | Eiffel
+               | PM
+               | SquareCube [Int]
+               | Banana
+               | Prime [Int]
+               | Spain
   deriving Show
 
 parseRequest :: Text -> Either ParseError Command
 parseRequest txt =
-  let parser = choice [
-        addParser,
-        nameParser
+  let parser = headerParser *> choice [
+        try largestParser,
+        try addParser,
+        try multParser,
+        try minusParser,
+        try eiffelParser,
+        try primeMinisterParser,
+--        try squareCubeParser,
+        try bananaParser,
+        try primeParser,
+        try spainParser
         ]
   in parse parser "parseRequest" txt
 
--- | addition parser
--- >>> parse addParser "test" "what is the sum of 12 and 5 ?"
--- Right (Add 12 5)
+
+intParser :: Parser Int
+intParser = read <$> many1 digit
+
+numbersParser :: Parser [Int]
+numbersParser = intParser `sepBy` string ", "
+
+eiffelParser :: Parser Command
+eiffelParser = Eiffel <$ string "which city is the Eiffel tower in"
+
+bananaParser :: Parser Command
+bananaParser = Banana <$ string "what colour is a banana"
+
+primeMinisterParser :: Parser Command
+primeMinisterParser = PM <$ string "who is the Prime Minister of Great Britain"
+
 addParser :: Parser Command
-addParser =
-  Add <$>
-  (string "what is the sum of " *> intParser <* string " and ") <*>
-  intParser <* string " ?"
-  where
-    intParser :: Parser Int
-    intParser = read <$> many1 digit
+addParser = Add <$> (string "what is " *> intParser) <*> (string " plus " *> intParser)
 
-nameParser :: Parser Command
-nameParser = Name <$ headerParser <* string "what is your name"
+minusParser :: Parser Command
+minusParser = Minus <$> (string "what is " *> intParser) <*> (string " minus " *> intParser)
 
+multParser :: Parser Command
+multParser = Mult <$> (string "what is " *> intParser) <*> (string " multiplied by " *> intParser)
+
+largestParser :: Parser Command
+largestParser = Largest <$> (string "which of the following numbers is the largest: " *> numbersParser)
+
+primeParser :: Parser Command
+primeParser = Prime <$> (string "which of the following numbers are primes: " *> numbersParser)
+
+squareCubeParser :: Parser Command
+squareCubeParser = SquareCube <$> (string "which of the following numbers is both a square and a cube: " *> numbersParser)
+
+spainParser :: Parser Command
+spainParser = Spain <$ string "what currency did Spain use before the Euro"
 
 -- | parse header
 -- >>> parse headerParser "test" "0041f190: "
@@ -47,9 +85,23 @@ headerParser :: Parser String
 headerParser =  many1 (oneOf "0123456789abcdef") <* string ": "
 
 -- | command evaluator
--- >>> evalCommand $ Add 10 15
--- "25"
 evalCommand :: Command -> Text
 evalCommand c = case c of
-  Add x y ->  T.pack $ show $ x + y
-  Name -> "JB"
+    Largest xs -> T.pack $ show $ maximum xs
+    Add a b -> T.pack $ show  (a  + b)
+    Mult a b -> T.pack $ show (a * b)
+    Minus a b -> T.pack $ show (a - b) 
+    Eiffel -> T.pack "Paris"
+    PM -> T.pack "David Cameron"
+--    SquareCube xs -> T.pack $ show $ head xs
+    Banana -> T.pack "yellow"
+    Spain -> T.pack "peseta"
+    Prime xs -> T.pack $ intercalate  ", " $  map show $ filter isPrime xs
+
+
+
+-- | isPrime
+-- >>> filter isPrime [2..10]
+-- [2,3,5,7]
+isPrime :: Int -> Bool
+isPrime n =  null [m | m <- [2..n-1], n `mod` m == 0]
