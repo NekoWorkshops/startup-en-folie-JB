87601201fe6f7026f6d58e590d4c018b2a08491e
Jean-Baptiste Potonnier <jean.baptiste.potonnier@gmail.com>
2015-03-24 14:05:35 +0100

    Warmup : parse header

--- src/Command.hs
+++ src/Command.hs
@@ -7,16 +7,21 @@ module Command (
 
 import           Data.Text        (Text)
 import qualified Data.Text        as T
-import           Text.Parsec      (ParseError, digit, many1, parse, string)
+import           Text.Parsec      (ParseError, digit, many1, parse, string, choice,
+                                   oneOf)
 import           Text.Parsec.Text (Parser)
-import Control.Applicative ((<$>), (*>), (<*), (<*>))
+import Control.Applicative ((<$>), (<$), (*>), (<*), (<*>))
 
 data Command = Add Int Int
+               | Name
   deriving Show
 
 parseRequest :: Text -> Either ParseError Command
 parseRequest txt =
-  let parser = addParser
+  let parser = choice [
+        addParser,
+        nameParser
+        ]
   in parse parser "parseRequest" txt
 
 -- | addition parser
@@ -30,10 +35,21 @@ addParser =
   where
     intParser :: Parser Int
     intParser = read <$> many1 digit
-    
+
+nameParser :: Parser Command
+nameParser = Name <$ headerParser <* string "what is your name"
+
+
+-- | parse header
+-- >>> parse headerParser "test" "0041f190: "
+-- Right "0041f190"
+headerParser :: Parser String
+headerParser =  many1 (oneOf "0123456789abcdef") <* string ": "
+
 -- | command evaluator
 -- >>> evalCommand $ Add 10 15
 -- "25"
 evalCommand :: Command -> Text
 evalCommand c = case c of
   Add x y ->  T.pack $ show $ x + y
+  Name -> "JB"
